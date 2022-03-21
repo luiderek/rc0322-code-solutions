fdc569a0f17dfdda61fa396a60abf7a87408e9a6
Derek Lui <luiderek@outlook.com>
2022-03-21 17:13:08 +0000

    javascript-primitives-and-variables complete

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+const fullName = 'Derek Lui';
+const isCool = false;
+const totalPets = 10;
+
+console.log('fullName:', fullName);
+console.log('isCool:', isCool);
+console.log('totalPets:', totalPets);
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+
+const unknown = null;
+
+console.log('unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+let never;
+console.log('never:', never);
+console.log('typeof never:', typeof never);
