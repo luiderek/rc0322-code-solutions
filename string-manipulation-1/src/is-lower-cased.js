0856e647b63fe9cc2df93e7e3292395de8dad8aa
Derek Lui <luiderek@outlook.com>
2022-03-25 00:09:16 +0000

    string-manipulation-1 complete

---
+++ string-manipulation-1/src/is-lower-cased.js
@@ -0,0 +1,7 @@
+/* exported isLowerCased */
+
+function isLowerCased(word) {
+  return word === word.toLowerCase();
+}
+
+// return whether the word is the same as a lowercase version of itself
---
+++ string-manipulation-1/src/is-vowel.js
@@ -0,0 +1,9 @@
+/* exported isVowel */
+
+function isVowel(char) {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  return vowels.includes(char);
+}
+
+// get a list of vowels
+// does the list of vowels contian what was entered?
