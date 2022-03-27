f4bf64d7340277c3f9b3f6201e6fdffb24c5eb68
Derek Lui <luiderek@outlook.com>
2022-03-27 22:36:48 +0000

    array-manipulation-3 complete

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,17 @@
 /* exported intersection */
+
+function intersection(first, second) {
+  const out = [];
+  for (const value of first) {
+    if (second.includes(value)) {
+      out.push(value);
+    }
+  }
+  return out;
+}
+
+// have empty output array
+// iterate through the values in the first list
+//    check if the second list includes those values
+//    if anything does overlap append to output array
+// return the output array
--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,8 @@
 /* exported union */
+
+function union(first, second) {
+  return [...new Set([...first, ...second])];
+}
+
+// i think concatenating the two then doing a unique filter would do it
+// concatenating might not be the right word for it, appending the arrays?
