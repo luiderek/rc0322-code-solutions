4ffc184c970216fb9ac3c010e148c8803645d8fc
Derek Lui <luiderek@outlook.com>
2022-03-28 20:09:33 +0000

    string-manipulation-3 complete

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -10,3 +10,8 @@ function reverseWords(string) {
   const reversedWords = words.map(reverseWord, words);
   return reversedWords.join(' ');
 }
+
+// seperate the words into an array
+// write a function that flips words arounds
+// call that function onto each word in the array
+// put the array back together with spaces in between
