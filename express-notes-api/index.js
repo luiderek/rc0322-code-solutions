f2f1be54c48e2b44024a8584d5057a28f03aaa79
Derek Lui <luiderek@outlook.com>
2022-05-11 19:55:01 +0000

    replacing trycatch with fs.writes, loading internal data object from require

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,53 +1,65 @@
+const data = require('./data.json');
+const fs = require('fs');
 const express = require('express');
-const app = express();
+
 const PORT = 3000;
-
-let nextId = 1;
-const notes = {};
-
+const app = express();
 const JSONparser = express.json();
 app.use(JSONparser);
 
 app.get('/api/notes/:id', (req, res) => {
   if (+req.params.id !== ~~req.params.id || +req.params.id <= 0) {
     res.status(400).send({ error: 'id must be a positive integer' });
-  } else if (notes[req.params.id]) {
-    res.status(200).send(notes[req.params.id]);
-  } else {
+  } else if (!data.notes[req.params.id]) {
     res.status(404).send({ error: `cannot find note with id ${req.params.id}` });
+  } else {
+    res.status(200).send(data.notes[req.params.id]);
   }
 });
 
 app.get('/api/notes/', (req, res) => {
-  res.send(noteArray(notes));
+  const noteArray = [];
+  for (const id in data.notes) {
+    noteArray.push(data.notes[id]);
+  }
+  res.status(200).send(noteArray);
 });
 
 app.post('/api/notes', (req, res) => {
-  if (!Object.keys(req.body).includes('content')) {
+  if (!req.body.content) {
     res.status(400).send({ error: 'content is a required field' });
   } else {
-    try {
-      notes[nextId] = {
-        content: req.body.content,
-        id: nextId
-      };
-      res.status(201).send(notes[nextId]);
-      nextId++;
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ error: 'an unexpected error occured' });
-    }
+    data.notes[data.nextId] = {
+      content: req.body.content,
+      id: data.nextId
+    };
+    fs.writeFile('data.json', JSON.stringify(data), err => {
+      if (err) {
+        console.error(err);
+        res.status(500).send({ error: 'an unexpected error occured' });
+      } else {
+        res.status(201).send(data.notes[data.nextId]);
+        data.nextId++;
+      }
+    });
   }
 });
 
 app.delete('/api/notes/:id', (req, res, next) => {
   if (+req.params.id !== ~~req.params.id || +req.params.id <= 0) {
     res.status(400).send({ error: 'id must be a positive integer' });
-  } else if (notes[req.params.id]) {
-    delete notes[req.params.id];
-    res.sendStatus(204);
-  } else {
+  } else if (!data.notes[req.params.id]) {
     res.status(404).send({ error: `cannot find note with id ${req.params.id}` });
+  } else {
+    delete data.notes[req.params.id];
+    fs.writeFile('data.json', JSON.stringify(data), err => {
+      if (err) {
+        console.error(err);
+        res.status(500).send({ error: 'an unexpected error occured' });
+      } else {
+        res.sendStatus(204);
+      }
+    });
   }
 });
 
@@ -56,18 +68,20 @@ app.put('/api/notes/:id', (req, res, next) => {
     res.status(400).send({ error: 'id must be a positive integer' });
   } else if (!Object.keys(req.body).includes('content')) {
     res.status(400).send({ error: 'content is a required field' });
-  } else if (!notes[req.params.id]) {
+  } else if (!data.notes[req.params.id]) {
     res.status(404).send({
       error: `cannot find note with id ${req.params.id}`
     });
   } else {
-    try {
-      notes[req.params.id].content = req.body.content;
-      res.status(200).send(notes[req.params.id]);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ error: 'an unexpected error occured' });
-    }
+    data.notes[req.params.id].content = req.body.content;
+    fs.writeFile('data.json', JSON.stringify(data), err => {
+      if (err) {
+        console.error(err);
+        res.status(500).send({ error: 'an unexpected error occured' });
+      } else {
+        res.status(200).send(data.notes[req.params.id]);
+      }
+    });
   }
 });
 
@@ -75,11 +89,3 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server is listening at port ${PORT}`);
 });
-
-function noteArray(noteObj) {
-  const out = [];
-  for (const key in noteObj) {
-    out.push(noteObj[key]);
-  }
-  return out;
-}
