8c299bbf03938996dc7077389535aad0c2f1cc64
Derek Lui <luiderek@outlook.com>
2022-05-18 22:35:44 +0000

    doing it the more react-y way

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function CustomButton(props) {
-  return <button> {props.action} Me! </button>;
+  // return <button> {props.action} Me! </button>;
+  return <button> Click Me! </button>;
 }
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
 );
 
-root.render(CustomButton({ action: 'Boop' }));
+// root.render(CustomButton({ action: 'Boop' }));
+root.render(<CustomButton />);
